ef06c42c4f15732266fd74e6b7342c864ca29bf9
ABM-BRS <chayk@hotmail.co.uk>
2026-03-19 11:52:26 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,20 @@
-const CACHE_NAME = "crew-app-cache-v1";
+const CACHE_NAME = 'crew-app-cache-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./favicon.png",
-  "./manifest.json",
-  "./alert.mp3",
-  "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js",
-  "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js"
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './alert.mp3'
 ];
 
-// Install event: cache files
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event: clean old caches
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if(key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
-});
-
-// Fetch event: serve cached files if offline
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
